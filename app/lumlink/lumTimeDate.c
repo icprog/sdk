580fe1b1e4619ac9fe6cc2ccc4a33f49403ecdec
mys_LumLink <rnd1@lumlink.com>
2015-02-07 13:55:51 +0800

    优化NTP保护机制

--- app/lumlink/lumTimeDate.c
+++ app/lumlink/lumTimeDate.c
@@ -22,16 +22,15 @@
 static TIME_DATE_INFO g_timeDateInfo;
 static os_timer_t g_systemTimeTimer;
 static os_timer_t g_networkTimeTimer;
-static os_timer_t g_getTimeTimer;
 
 static BOOL g_getUTCSucc;
+static BOOL g_needDisconnect;
 
 static struct espconn timeConnHandle;
 static struct _esp_tcp timeTcpHandle;
 
 
-static void USER_FUNC lum_getNetworkTime(void);
-static void USER_FUNC lum_syncNetworkTimer(U32 timeGap);
+static void USER_FUNC lum_syncNetworkTimer(BOOL protectCallBack, U32 timeGap);
 
 
 
@@ -44,36 +43,17 @@ static void USER_FUNC lum_syncNetworkTime(U32 networkTime)
 }
 
 
-static void USER_FUNC lum_timeSocketProtect(ETSTimerFunc pfunction)
-{
-	os_timer_disarm(&g_getTimeTimer);
-	os_timer_setfn(&g_getTimeTimer, pfunction, NULL);
-	os_timer_arm(&g_getTimeTimer, TIME_RECONNECT_TIMER_GAP, 0);
-}
-
-
-
-// set timeout while connect but not get time
-static void USER_FUNC lum_disconnectTimerCallback(void *arg)
-{
-	espconn_disconnect(&timeConnHandle);
-	if(!g_getUTCSucc)
-	{
-		lum_syncNetworkTimer(SYNC_NETWORK_TIME_TIMER_FAILD_GAP); //获取时间失败，减短等待时间
-	}
-}
-
-
-
 static void USER_FUNC lum_timeRecvCallback(void *arg, char *pusrdata, unsigned short length)
 {
 	U32* tmp;
 	U32 curUTC;
+	struct espconn *pespconn = arg;
 
 
 	tmp = (U32*)pusrdata;
 	curUTC = ntohl(tmp[0]);
 	lum_syncNetworkTime(curUTC);
+	espconn_disconnect(pespconn);
 	g_getUTCSucc = TRUE;
 }
 
@@ -94,42 +74,23 @@ static void USER_FUNC lum_timeConnectCallback(void *arg)
 	lumDebug("lum_timeConnectCallback\n");
 	espconn_regist_recvcb(pespconn, lum_timeRecvCallback);
 	espconn_regist_sentcb(pespconn, lum_timeSentCallback);
-
-	lum_timeSocketProtect(lum_disconnectTimerCallback);//链接后10秒，如没有断开则强制断开
-}
-
-
-static void USER_FUNC lum_reconnectTimerCallback(void *arg)
-{
-	lum_getNetworkTime();
+	g_needDisconnect = TRUE;
 }
 
 
 static void USER_FUNC lum_timeDisconnectCallback(void *arg)
 {
-	struct espconn *pespconn = arg;
-
+	//struct espconn *pespconn = arg;
 
 	lumDebug("lum_timeDisconnectCallback \n");
-	os_timer_disarm(&g_getTimeTimer); //如已经断开，取消保护
-	espconn_delete(pespconn);
+	g_needDisconnect = FALSE;
 }
 
 
 static void USER_FUNC lum_timeReconnectCallback(void *arg, sint8 err)
 {
-	struct espconn *pespconn = (struct espconn *)arg;
-
-
 	lumDebug("lum_timeReconnectCallback err=%d\n", err);
-	if(g_getUTCSucc)
-	{
-		lum_timeDisconnectCallback(pespconn);
-	}
-	else
-	{
-		lum_timeSocketProtect(lum_reconnectTimerCallback);
-	}
+	g_needDisconnect = FALSE;
 }
 
 
@@ -156,6 +117,8 @@ static void USER_FUNC lum_getNetworkTime(void)
 
 	espconn_connect(&timeConnHandle);
 	g_getUTCSucc = FALSE;
+	g_needDisconnect = FALSE;
+	lum_syncNetworkTimer(TRUE, SYNC_NETWORK_TIME_TIMER_PROTECT_GAP);
 }
 
 
@@ -197,15 +160,48 @@ static void USER_FUNC lum_syncSystemTimer(void)
 
 static void USER_FUNC lum_syncNetworkTimerCallback(void *arg)
 {
-	lum_syncNetworkTimer(SYNC_NETWORK_TIME_TIMER_SUCC_GAP);
 	lum_getNetworkTime();
 }
 
 
-static void USER_FUNC lum_syncNetworkTimer(U32 timeGap)
+static void USER_FUNC lum_protectNetworkTimerCallback(void *arg)
 {
+	lumDebug("lum_protectNetworkTimerCallback g_needDisconnect=%d g_getUTCSucc=%d\n", g_needDisconnect, g_getUTCSucc);
+	if(g_needDisconnect)
+	{
+		espconn_disconnect(&timeConnHandle);
+		lum_syncNetworkTimer(TRUE, SYNC_NETWORK_TIME_TIMER_PROTECT_GAP); //wait disconnect
+	}
+	else
+	{
+		espconn_delete(&timeConnHandle);
+		if(g_getUTCSucc)
+		{
+			lum_syncNetworkTimer(FALSE, SYNC_NETWORK_TIME_TIMER_SUCC_GAP); //start nect sync timer
+		}
+		else
+		{
+			lum_syncNetworkTimer(FALSE, SYNC_NETWORK_TIME_TIMER_PROTECT_GAP);
+		}
+	}
+}
+
+
+static void USER_FUNC lum_syncNetworkTimer(BOOL protectCallBack, U32 timeGap)
+{
+	ETSTimerFunc* pfunction;
+
+
+	if(protectCallBack)
+	{
+		pfunction = lum_protectNetworkTimerCallback;
+	}
+	else
+	{
+		pfunction = lum_syncNetworkTimerCallback;
+	}
 	os_timer_disarm(&g_networkTimeTimer);
-	os_timer_setfn(&g_networkTimeTimer, (os_timer_func_t *)lum_syncNetworkTimerCallback, NULL);
+	os_timer_setfn(&g_networkTimeTimer, (os_timer_func_t *)pfunction, NULL);
 	os_timer_arm(&g_networkTimeTimer, timeGap, 0);
 }
 
@@ -222,7 +218,7 @@ void USER_FUNC lum_initSystemTime(void)
 //init network time after DHPC
 void USER_FUNC lum_initNetworkTime(void)
 {
-	lum_syncNetworkTimer(10000);
+	lum_syncNetworkTimer(FALSE, SYNC_NETWORK_TIME_TIMER_PROTECT_GAP);
 }
 
 
